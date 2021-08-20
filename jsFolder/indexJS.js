a758f3e482f0e91bf0cdf7963b9d761e30420227
daxiong2009 <georgeyu.mn@gmail.com>
2021-08-19 23:39:38 -0700

    ...

---
+++ jsFolder/indexJS.js
@@ -0,0 +1,33 @@
+
+function bio_text(){
+    var bio = document.getElementById("buttonClick");
+    var str1 = "欢迎来到我的个人站。 我是XX运动康复工作室的创立者。我专注于骨骼肌肉损伤的康复和运动损伤的预防。"+
+    "我拥有美国华盛顿州卫生部的运动损伤防护师执照。自2015年获得执照起，我服务过多个学校的运动队和医院，"+
+    "包括明尼苏达大学，西曼开度中学和西雅图儿童医院等。"
+    bio.innerHTML = str1;
+}
+
+function edu_text(){
+    var edu = document.getElementById("buttonClick");
+    var str2 = "我于2021年在美国华盛顿大学康复医学系"+
+    "(Department of Rehabilitation Medicine, University of Washington)"+
+    "完成了康复科学的博士学位 (PhD in rehabilitation science)。我的研究兴趣包括临床步态诊断与分析，"+
+    "身体形态对运动中神经肌肉控制的影响，走路和跑步的能量消耗，以及大数据驱动下的运动损伤预防。"+
+    "我的研究方法主要基于现代生物力学技术。我从本科时期开始就对工程与数学很感兴趣。"+
+    "这份兴趣在我从事临床康复治疗工作之后并未减弱，并最终促使我进入了康复医学和生物力学的研究领域。"+
+    "我在2020年获得了华盛顿大学的应用数学硕士学位(MS in Applied Mathematics)。";
+    edu.innerHTML = str2;
+}
+
+function sport_text(){
+    var sport = document.getElementById("buttonClick");
+    var str3 = "我从初中起在体育运动学校进行拳击训练，之后进入了上海体育学院的进行本科学习"+
+    "这份经历除了让我能更深入地了解运动损伤发生的原理，也让我一直保持着规律运动的习惯。"+
+    "规律运动对我来说是生活中不可或缺的一部分，它让我能够有质量的生活和工作。"+
+    "我相信每一个运动爱好者都和我是帮助每一位患者解除疼痛，或是术后下地步行，"+
+    "更是让每一位体育运动爱好者安全地、尽快地回到运动场，享受运动带来的生活乐趣。"
+    sport.innerHTML = str3;
+}
+
+
+
